03825d002e8bea98c5bcfa4de2319af27fdafeb8
Phaveey <favourattah80@gmail.com>
2022-12-14 19:05:44 -0800

    Checking for alphabetical characters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,21 @@
 
 /**
  * _isalpha - Return 1 if c is a letter (lowercase or uppercase)
- * @c: an unsigned int value to be compared with ASCII value
+ *
+ * @c: an unsigned int value to be printed
  * Return: 1 if it is alphabet. If not,return 0
  */
 
 int _isalpha(int c)
+
 {
-	if (c >= 65; && c <= 122)
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
 		return (1);
+	}
+
 	else
+	{
 		return (0);
+	}
 }
